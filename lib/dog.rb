a6764475ea17e4d51db44bc3ee6cc0888bf10dae
Tammy Metz <tammy@tammymetz.com>
2018-09-23 15:13:43 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -42,6 +42,18 @@ class Dog
     end.first
   end
 
+  def self.find_by_id(id)
+    sql = <<-SQL
+      SELECT * FROM dogs
+      WHERE id = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
+
   def update
     sql = "UPDATE dogs SET name = ?, breed = ? WHERE id=?;"
     DB[:conn].execute(sql, self.name, self.breed, self.id)
